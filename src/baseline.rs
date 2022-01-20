5566fdfe56140362e05a29c43080ef541f436eb2
Sam Rijs <srijs@airpost.net>
2022-01-20 15:06:19 +0100

    prevent table stack copy in debug mode
    While investigating poor performance in debug mode, it appears
    that `rustc` will compile `a[b][c]` by first copying the return
    value of `a.index(b)` onto the stack before indexing it via `c`.
    
    In our case, this results in at least one 1KB `memmove` of CRC32
    table sections per input chunk processed, slowing down throughput
    quite significantly.
    
    Luckily the fix appears to be as simple as replacing `a[b][c]`
    with `(&a[b])[c]`.

--- src/baseline.rs
+++ src/baseline.rs
@@ -35,22 +35,22 @@ pub(crate) fn update_fast_16(prev: u32, mut buf: &[u8]) -> u32 {
 
     while buf.len() >= BYTES_AT_ONCE {
         for _ in 0..UNROLL {
-            crc = CRC32_TABLE[0x0][buf[0xf] as usize]
-                ^ CRC32_TABLE[0x1][buf[0xe] as usize]
-                ^ CRC32_TABLE[0x2][buf[0xd] as usize]
-                ^ CRC32_TABLE[0x3][buf[0xc] as usize]
-                ^ CRC32_TABLE[0x4][buf[0xb] as usize]
-                ^ CRC32_TABLE[0x5][buf[0xa] as usize]
-                ^ CRC32_TABLE[0x6][buf[0x9] as usize]
-                ^ CRC32_TABLE[0x7][buf[0x8] as usize]
-                ^ CRC32_TABLE[0x8][buf[0x7] as usize]
-                ^ CRC32_TABLE[0x9][buf[0x6] as usize]
-                ^ CRC32_TABLE[0xa][buf[0x5] as usize]
-                ^ CRC32_TABLE[0xb][buf[0x4] as usize]
-                ^ CRC32_TABLE[0xc][buf[0x3] as usize ^ ((crc >> 0x18) & 0xFF) as usize]
-                ^ CRC32_TABLE[0xd][buf[0x2] as usize ^ ((crc >> 0x10) & 0xFF) as usize]
-                ^ CRC32_TABLE[0xe][buf[0x1] as usize ^ ((crc >> 0x08) & 0xFF) as usize]
-                ^ CRC32_TABLE[0xf][buf[0x0] as usize ^ ((crc >> 0x00) & 0xFF) as usize];
+            crc = (&CRC32_TABLE[0x0])[buf[0xf] as usize]
+                ^ (&CRC32_TABLE[0x1])[buf[0xe] as usize]
+                ^ (&CRC32_TABLE[0x2])[buf[0xd] as usize]
+                ^ (&CRC32_TABLE[0x3])[buf[0xc] as usize]
+                ^ (&CRC32_TABLE[0x4])[buf[0xb] as usize]
+                ^ (&CRC32_TABLE[0x5])[buf[0xa] as usize]
+                ^ (&CRC32_TABLE[0x6])[buf[0x9] as usize]
+                ^ (&CRC32_TABLE[0x7])[buf[0x8] as usize]
+                ^ (&CRC32_TABLE[0x8])[buf[0x7] as usize]
+                ^ (&CRC32_TABLE[0x9])[buf[0x6] as usize]
+                ^ (&CRC32_TABLE[0xa])[buf[0x5] as usize]
+                ^ (&CRC32_TABLE[0xb])[buf[0x4] as usize]
+                ^ (&CRC32_TABLE[0xc])[buf[0x3] as usize ^ ((crc >> 0x18) & 0xFF) as usize]
+                ^ (&CRC32_TABLE[0xd])[buf[0x2] as usize ^ ((crc >> 0x10) & 0xFF) as usize]
+                ^ (&CRC32_TABLE[0xe])[buf[0x1] as usize ^ ((crc >> 0x08) & 0xFF) as usize]
+                ^ (&CRC32_TABLE[0xf])[buf[0x0] as usize ^ ((crc >> 0x00) & 0xFF) as usize];
             buf = &buf[16..];
         }
     }
@@ -62,7 +62,7 @@ pub(crate) fn update_slow(prev: u32, buf: &[u8]) -> u32 {
     let mut crc = !prev;
 
     for &byte in buf.iter() {
-        crc = CRC32_TABLE[0][((crc as u8) ^ byte) as usize] ^ (crc >> 8);
+        crc = (&CRC32_TABLE[0])[((crc as u8) ^ byte) as usize] ^ (crc >> 8);
     }
 
     !crc
